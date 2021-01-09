e0c3fca7cf8be28764fc062306f117553fd28e10
M_788192 <m_788192@ambus54920.home>
2021-01-09 13:12:20 -0600

    empty project created

---
+++ src/main/java/com/galvanize/cryptozoology/CryptozoologyApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.cryptozoology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptozoologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptozoologyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galvanize/cryptozoology/CryptozoologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.cryptozoology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptozoologyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
